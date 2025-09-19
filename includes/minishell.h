e1e21aba14962f2f4bf0f4d69af19dc6de2a9a95
rossnotfromfrients <rdolgopolovv@gmail.com>
2025-09-19 15:57:06 +0200

    1 version of tokenizer

---
+++ includes/minishell.h
@@ -0,0 +1,48 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "../libft/libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <signal.h>
+# include <dirent.h>
+# include <termios.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <errno.h>
+
+# define SUCCESS 0
+# define ERROR 1
+
+/*
+ * Global variable to handle signals
+ * As per subject: Just one global variable to store signal number
+*/
+extern int	g_sig_status;
+
+/* Token types */
+typedef enum e_token_type
+{
+	TOKEN_WORD,
+	TOKEN_PIPE,
+	TOKEN_REDIR_IN,
+	TOKEN_REDIR_OUT,
+	TOKEN_REDIR_APPEND,
+	TOKEN_HEREDOC,
+	TOKEN_AND,
+	TOKEN_OR,
+	TOKEN_PAREN_OPEN,
+	TOKEN_PAREN_CLOSE
+}	t_token_type;
+
+/* Token structure */
+typedef struct s_token
+{
+	char			*value;
+	t_token_type	type;
+	struct s_token	*next;
+}	t_token;
---
+++ srcs/tokenizer.c
@@ -0,0 +1,78 @@
+#include "../includes/minishell.h"
+
+/**
+ * Creates a new token structure
+ */
+t_token	*create_token(char *value, t_token_type type)
+{
+	t_token	*token;
+
+	token = (t_token *)malloc(sizeof(t_token));
+	if (!token)
+		return (NULL);
+	token->value = ft_strdup(value);
+	if (!token->value)
+	{
+		free(token);
+		return (NULL);
+	}
+	token->type = type;
+	token->next = NULL;
+	return (token);
+}
+
+/**
+ * Adds a token to the end of the token list
+ */
+void	add_token(t_token **head, t_token *new_token)
+{
+	t_token	*temp;
+
+	if (!*head)
+		*head = new_token;
+	else
+	{
+		temp = *head;
+		while (temp->next)
+			temp = temp->next;
+		temp->next = new_token;
+	}
+}
+
+/**
+ * Main tokenization function
+ * Converts input string into a linked list of tokens
+ */
+t_token	*tokenize_input(char *input)
+{
+	t_token			*tokens;
+	t_token			*new_token;
+	t_token_type	type;
+	int				i;
+	char			*value;
+
+	tokens = NULL;
+	i = 0;
+	while (input[i])
+	{
+		while (ft_isspace(input[i]))
+			i++;
+		if (!input[i])
+			break ;
+		if (ft_strchr("|<>&()", input[i]))
+		{
+			type = identify_token_type(input, &i);
+			value = ft_strdup("");
+			i++;
+		}
+		else
+		{
+			type = TOKEN_WORD;
+			value = extract_word(input, &i);
+		}
+		new_token = create_token(value, type);
+		free(value);
+		add_token(&tokens, new_token);
+	}
+	return (tokens);
+}
