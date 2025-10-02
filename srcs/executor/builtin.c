517df4646962bf96cbc72753333069326a734b1c
personal-laptop <ceske@etik.com>
2025-10-02 19:34:03 +0200

    Added builtins: pwd, env

--- srcs/executor/builtin.c
+++ srcs/executor/builtin.c
@@ -1,6 +1,35 @@
 #include "executor.h"
+#include "ft_printf.h"
 
-void change_directory(char *dirname)
+
+void	env()
+{
+	extern const char	**environ;
+	int					i;
+
+	i = 0;
+	while(environ[i])
+	{
+		ft_printf("%s\n", environ[i]);
+		i++;
+	}
+	
+}
+
+void	echo(char *str, bool flag_n) 
+{
+	if (flag_n)
+		ft_printf("%s", str);
+	else
+		ft_printf("%s\n", str);
+}
+
+void	print_workig_directory()
+{	
+	ft_printf("%s\n", getcwd(NULL, 0));
+}
+
+void	change_directory(char *dirname)
 {
 	// Need to manage the "too many arguments" case.
 	// idk how to do it before the end of tokenizer
@@ -16,3 +45,4 @@ void change_directory(char *dirname)
 	if (chdir(dirname) != 0)
 		perror(ft_strjoin("cd: ", dirname));
 }
+
