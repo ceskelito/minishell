7a82595f319b8e3a88751028f2ac04945becac4f
ceske-42 <ceske@etik.com>
2025-10-29 11:31:54 +0100

    Removed comments

--- srcs/parser/parser_utils.c
+++ srcs/parser/parser_utils.c
@@ -5,7 +5,6 @@
 
 int	is_redir_token(t_token_type type)
 {
-	//return (type & IN || type & OUT || type & APPEND || type & HEREDOC);
 	return (type & (IN | OUT | APPEND | HEREDOC));
 }
 
@@ -43,52 +42,3 @@ int	parse_redirection(t_cmd *cmd, t_token **token)
 	return (0);
 }
 
-/* NOT USED 
-int	cmd_count_args(char **args)
-{
-	int	count;
-
-	count = 0;
-	if (!args)
-		return (0);
-	while (args[count])
-		count++;
-	return (count);
-} */
-
-//static char	**allocate_new_args(int count)
-//{
-//	char	**new_args;
-//
-//	new_args = ezg_alloc(GLOBAL, sizeof(char *) * (count + 2));
-//	return (new_args);
-//}
-
-/* NOT USED
-int	add_arg(char **args, char *arg)
-{
-	int		count;
-	char	**new_args;
-	int		i;
-
-	count = cmd_count_args(args);
-	new_args = ezg_alloc(COMMAND, sizeof(char *) * (count + 2));
-	if (!new_args)
-		return (0);
-	i = 0;
-	while (i < count)
-	{
-		new_args[i] = args[i];
-		i++;
-	}
-	new_args[i] = ezg_add(GLOBAL, ft_strdup(arg));
-	if (!new_args[i])
-	{
-		//free(new_args);
-		return (0);
-	}
-	new_args[i + 1] = NULL;
-	ezg_release(COMMAND, args);
-	args = new_args;
-	return (1);
-} */
