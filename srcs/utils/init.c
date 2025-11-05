75ff1704768fb2a1bf4f8984207601fd9e5199a6
ceske-42 <ceske@etik.com>
2025-11-05 17:22:56 +0100

    Created: init.c - File in which store the initialization functions

---
+++ srcs/utils/init.c
@@ -0,0 +1,50 @@
+#include "minishell.h"
+
+void	init_shell(t_shell *shell, char **envp)
+{
+	shell->env_list = NULL;
+	shell->cmd_list = NULL;
+	shell->tokens = NULL;
+	shell->env_array = envp;
+	shell->line = NULL;
+	shell->exit_status = 0;
+	shell->interactive = 1;
+	shell->std_out = dup(STDOUT_FILENO);
+	shell->std_in = dup(STDIN_FILENO);
+}
+
+/**
+ * init_env - Duplicate the system environment into the ezgalloc-managed ENV group.
+ *
+ * This function copies all entries from the provided @environ array into
+ * a new NULL-terminated array allocated under the ezgalloc group ENV.
+ * The resulting array replaces the one referenced by ft_getenv_array(),
+ * making it available for later access and manipulation through the
+ * environment utility functions defined in utils/environment.c.
+ *
+ * @environ  Pointer to the system environment array (NULL-terminated).
+ *
+ * Return: None.
+ */
+void	init_env(char **environ)
+{
+	int		i;
+	int		nmemb;
+	char	**env;
+
+	env = ft_getenv_array();
+	if (!env)
+		return ;
+	nmemb = 0;
+	while (environ[nmemb])
+		nmemb++;
+	env = ezg_calloc(ENV, sizeof(char *), nmemb + 1);
+	i = 0;
+	while (environ[i])
+	{
+		env[i] = ft_strdup(environ[i]);
+		ezg_add(ENV, env[i]);
+		i++;
+	}
+    env[i] = NULL;
+}
