80fc19e6e9e6b9b9503cd413639ae71e83bc70f7
personal-laptop <ceske@etik.com>
2025-11-14 12:04:17 +0100

    moved is_bultin in utils

--- srcs/utils/general_utils.c
+++ srcs/utils/general_utils.c
@@ -8,3 +8,11 @@ int print_error(char *msg, char *err)
 					"minishell: %s: %s\n", msg, err);
     return (len);
 }
+
+bool	is_builtin(char *cmd)
+{
+	return (!ft_strcmp(cmd, "echo") || !ft_strcmp(cmd, "cd")
+		|| !ft_strcmp(cmd, "pwd") || !ft_strcmp(cmd, "env")
+		|| !ft_strcmp(cmd, "exit") || !ft_strcmp(cmd, "export")
+		|| !ft_strcmp(cmd, "unset"));
+}
