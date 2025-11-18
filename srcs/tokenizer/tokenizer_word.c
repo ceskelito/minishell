2144f3a9d78c84a2d0d3b7955aa4955425434779
ceske-42 <ceske@etik.com>
2025-11-18 18:33:15 +0100

    Rewrote the tokenization of words

--- srcs/tokenizer/tokenizer_word.c
+++ srcs/tokenizer/tokenizer_word.c
@@ -1,240 +1,103 @@
-#include "ft_lib.h"
 #include "minishell.h"
 
-static int	count_word_length(char *word, bool *expand_dollar, bool *cat_to_next)
-{
-	int		len;
-	int		in_quote;
-	char	quote_char;
+enum {NO_QUOTES = -2, UNCLOSED_QUOTES = -1};
+enum {IN_QUOTES = true, OUT_QUOTES = false};
 
-	len = 0;
-	in_quote = 0;
-	quote_char = 0;
-	while (word[len])
-	{
-		if (!in_quote)
-		{
-			if(ft_isspace(word[len])
-			|| ft_strchr("|<>&()", word[len])
-			|| (len != 0 && ft_strchr("'\'", word[len])))
-				break ;
-		}
-		if (in_quote && word[len] == quote_char)
-		{
-			*expand_dollar = !(quote_char == '\'');
-			word++;
-			in_quote = false;
-			break ;
-		}
-/* 		if (!in_quote && (ft_isspace(word[len]) || ft_strchr("|<>&()",
-					word[len])))
-			break ; */
-		if (!in_quote && (word[len] == '\'' || word[len] == '"'))
-		{
-			in_quote = 1;
-			quote_char = word[len];
-		}
-		else if (in_quote && word[len] == quote_char)
-		{
-			in_quote = 0;
-			quote_char = 0;
-		}
-		len++;
-	}
-		printf("word: %s\nat len: %s\n", word, word + len);
-	if (in_quote)
-	{
-		print_error("syntax error", "unclosed quote\n");
-		return (-1);
-	}
-	if (word[len] && !ft_isspace(word[len]))
-		*cat_to_next = true;
-	return (len);
+static inline bool	isquote(char c)
+{
+	return (c == '\'' || c == '"');	
 }
 
-/* static char	*handle_word_char(char *result, char c)
+static inline bool isspecial(char c)
 {
-	char	*temp;
-
-	temp = ft_strjoin_char(result, c);
-	if (!temp)
-		return (NULL);
-	free(result);
-	return (temp);
-} */
-
-/* static char	*process_single_quote(char *input, int *i, char *result)
-{
-	(*i)++;
-	while (input[*i] && input[*i] != '\'')
-	{
-		result = handle_word_char(result, input[*i]);
-		if (!result)
-			return (NULL);
-		(*i)++;
-	}
-	if (input[*i] == '\'')
-		(*i)++;
-	return (result);
+	return (ft_strchr("|<>", c));
 }
 
-static char	*process_double_quote(char *input, int *i, char *result)
-{
-	(*i)++;
-	while (input[*i] && input[*i] != '\"')
-	{
-		if (input[*i] == '$')
-		{
-			handle_dollar_sign(input, i, &result);
-			if (!result)
-				return (NULL);
-		}
-		else
-		{
-			result = handle_word_char(result, input[*i]);
-			if (!result)
-				return (NULL);
-			(*i)++;
-		}
-	}
-	if (input[*i] == '\"')
-		(*i)++;
-	return (result);
-} */
-
-/* static char	*expand_word_value(char *input, int len)
+static void	set_token_value(t_token *token, char *input, int gap, int len, bool in_quote)
 {
 	char	*result;
-	int		i;
 
-	result = ft_strdup("");
+	if (in_quote)
+		token->expand_dollar = false;	
+	if (input[gap + 1] && !ft_isspace(input[gap + 1]))
+		token->cat_to_next = true;
+	if (len == 0)
+		result = ft_strdup("");
+	else
+		result = ft_substr(input, in_quote, len);
 	if (!result)
-		return (NULL);
-	i = 0;
-	while (i < len)
 	{
-		if (input[i] == '\'')
-		{
-			result = process_single_quote(input, &i, result);
-			if (!result)
-				return (NULL);
-		}
-		else if (input[i] == '\"')
-		{
-			result = process_double_quote(input, &i, result);
-			if (!result)
-				return (NULL);
-		}
-		else if (input[i] == '$')
-		{
-			handle_dollar_sign(input, &i, &result);
-			if (!result)
-				return (NULL);
-		}
-		else
-		{
-			result = handle_word_char(result, input[i]);
-			if (!result)
-				return (NULL);
-			i++;
-		}
+		perror("minishell:");
+		exit(errno);
 	}
-	return (result);
-} */
-
-char *extract_word(char *input, char quote_char, int *len)
-{
-	int		i;
-	char	*value;
-
-	if (quote_char)
-		input++;
+	ezg_add(TOKEN, result);
+	token->value = result;
 	
+}
 
-	i = 1; // Skip the opening quote
-	while (input[i] && input[i] != quote_char)
-		i++;
-	*len = i + 1; // Include the closing quote
-	value = ft_substr(input, 1, i - 1); // Extract the content inside quotes
-	ezg_add(GLOBAL, value);
-	return (value);
+static int	process_word_surrounded(t_token *token, char *input)
+{
+	int		gap;
+	int		len;
+	char	quote;
+	char	*result;
+
+	gap = 1;
+	len = 0;
+	quote = input[0];
+	while (input[gap] && input[gap] != quote)
+	{
+		len++;
+		gap++;
+	}
+	gap++;
+	if (input[gap] != quote)
+	{
+		ft_dprintf(STDERR_FILENO, "minishell: unexpected EOF while looking for matching `%c", quote);
+		print_error("syntax error", "unexpected end of file");
+		return (UNCLOSED_QUOTES);
+	}
+	set_token_value(token, input, gap, len, IN_QUOTES);
+	return (gap);
+}
+
+int	process_word_nosurround(t_token *token, char *input)
+{
+	int		gap;
+	int		len;
+	char	*result;
+
+	gap = 0;
+	len = 0;
+	while (input[gap] && !( isquote(input[gap]) || ft_isspace(input[gap]) || isspecial(input[gap]) ))
+	{
+		len++;
+		gap++;
+	}
+	set_token_value(token, input, gap, len, OUT_QUOTES);
+	return (gap);
 }
 
 int	fill_word_token(t_token *token, char *input)
 {
 	int		len;
 	int		gap;
+	int		spaces;
 	char	*value;
-	
-	gap = 0;
+
+	spaces = 0;
 	while (ft_isspace(*input))
 	{	
-		gap++;
+		spaces++;
 		input++;
 	}
-	if (input[0] == '\'' || input[0] == '"')
-	{
-		value = extract_word();
-	}
-
-	len = count_word_length(input, &token->expand_dollar, &token->cat_to_next);
-	if (len == -1)
-		return (-1);
-	if (len == 0)
-	{
-		token->value = NULL;
-		token->type = WORD;
-		return (0);
-	}
-	//expanded = expand_word_value(input, len);
-	if (input[0] == '"' || input[0] == '\'')
-	{
-		input++;
-		len--;
-	}
-	token->value = ft_substr(input, 0, len);
-	ezg_add(GLOBAL, token->value);	
-	//printf("value: %s\nexpand: %d\nconcat:%d", token->value, token->expand_dollar, token->cat_to_next);
-	//exit(1);
-	/* if (!expanded)
-		return (-1); */
-	if (!token->value)
-		return (-1);
+	gap = 0;
+	if (isquote(input[0]))
+		gap = process_word_surrounded(token, input);
+	else
+		gap = process_word_nosurround(token, input);
+	if (gap < 0)
+		return (gap);
 	token->type = WORD;
-	return (len);
+	return (gap + spaces);
 }
-
-// int	fill_word_token(t_token *token, char *input)
-// {
-// 	int		len;
-// 	int		gap;
-// 	char	*value;
-// 	//char	*expanded;
-// 
-// /* 	value = extract_word_value(input, &len, &token->expand_dollar, &token->cat_to_next); */
-// 	len = count_word_length(input, &token->expand_dollar, &token->cat_to_next);
-// 	if (len == -1)
-// 		return (-1);
-// 	if (len == 0)
-// 	{
-// 		token->value = NULL;
-// 		token->type = WORD;
-// 		return (0);
-// 	}
-// 	//expanded = expand_word_value(input, len);
-// 	if (input[0] == '"' || input[0] == '\'')
-// 	{
-// 		input++;
-// 		len--;
-// 	}
-// 	token->value = ft_substr(input, 0, len);
-// 	ezg_add(GLOBAL, token->value);	
-// 	//printf("value: %s\nexpand: %d\nconcat:%d", token->value, token->expand_dollar, token->cat_to_next);
-// 	//exit(1);
-// 	/* if (!expanded)
-// 		return (-1); */
-// 	if (!token->value)
-// 		return (-1);
-// 	token->type = WORD;
-// 	return (len);
-// }
