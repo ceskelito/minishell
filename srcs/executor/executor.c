0ea6cde28ffa7988ab69bf3b6197dad9826bd4d6
personal-laptop <ceske@etik.com>
2025-11-20 16:15:21 +0100

    Moved some functions to comply the norme

--- srcs/executor/executor.c
+++ srcs/executor/executor.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   executor.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rceschel <rceschel@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rceschel <rceschel@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 12:42:14 by rceschel          #+#    #+#             */
-/*   Updated: 2025/11/14 16:34:07 by rceschel         ###   ########.fr       */
+/*   Updated: 2025/11/20 16:11:36 by rceschel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,74 @@ static int	count_cmds(t_cmd *cmd_list)
 		cmd_list = cmd_list->next;
 	}
 	return (count);
+}
+
+static void	execute_in_child(t_cmd *cmd, pid_t *pid, int (*exec_cmd)(const char *,
+			char *const[], char *const[]))
+{
+	bool	is_child;
+
+	*pid = fork();
+	is_child = !(*pid);
+	if (is_child)
+	{
+		close_pipe_fds(cmd->next, PIPE | HEREDOC, -1);
+		if (apply_redirs(cmd->redirs) != 0)
+			exit(errno);
+		if (resolve_command_location(cmd), cmd->location)
+			exec_cmd(cmd->location, cmd->args, ft_getenv_array());
+		print_error(cmd->args[0], strerror(errno));
+		exit(127);
+	}
+	else if (*pid > 0)
+		close_pipe_fds(cmd, PIPE | HEREDOC, 1);
+	else
+	{
+		perror("minishell");
+		set_exit_status(errno);
+	}
+}
+
+static int	execute_builtin(const char *pathname, char *const argv[],
+		char *const envp[])
+{
+	int	exit_value;
+
+	(void)envp;
+	if (!ft_strcmp(argv[0], "echo"))
+		echo(argv);
+	else if (!ft_strcmp(argv[0], "cd"))
+		cd(argv);
+	else if (!ft_strcmp(argv[0], "pwd"))
+		pwd();
+	else if (!ft_strcmp(argv[0], "env"))
+		env();
+	else if (!ft_strcmp(argv[0], "exit"))
+		exit_shell(argv);
+	else if (!ft_strcmp(argv[0], "export"))
+		export(argv);
+	else if (!ft_strcmp(argv[0], "unset"))
+		unset(argv);
+	else
+		exit_value = 1;
+	exit_value = 0;
+	if (ft_strcmp(pathname, "child") == 0)
+		exit(exit_value);
+	return (exit_value);
+}
+
+static void	execute_in_parent(t_shell *shell, t_cmd *cmd)
+{
+	int	exit_code;
+
+	if (apply_redirs(cmd->redirs) == 0)
+		exit_code = execute_builtin("parent", cmd->args, NULL);
+	else
+		exit_code = errno;
+	dup2(shell->std_in, STDIN_FILENO);
+	dup2(shell->std_out, STDOUT_FILENO);
+	set_exit_status(exit_code);
+	ezg_group_release(EXECUTING);
 }
 
 void	executor(t_shell *shell)
--- srcs/executor/redirections.c
+++ srcs/executor/redirections.c
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   exec_functions.c                                   :+:      :+:    :+:   */
+/*   redirections.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rceschel <rceschel@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 14:16:30 by rceschel          #+#    #+#             */
-/*   Updated: 2025/11/19 11:34:27 by rceschel         ###   ########.fr       */
+/*   Updated: 2025/11/20 16:14:35 by rceschel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executor.h"
 #include "minishell.h"
 
-static void	close_pipe_fds(t_cmd *cmd, int redir_type, int cmd_to_parse)
+void	close_pipe_fds(t_cmd *cmd, int redir_type, int cmd_to_parse)
 {
 	t_cmd	*curr_cmd;
 	t_redir	*curr_redir;
@@ -50,7 +50,7 @@ static int	open_redir(t_redir *r)
 	return (-1);
 }
 
-static int	apply_redirs(t_redir *r)
+int	apply_redirs(t_redir *r)
 {
 	int		fd;
 
@@ -67,72 +67,4 @@ static int	apply_redirs(t_redir *r)
 		r = r->next;
 	}
 	return (0);
-}
-
-void	execute_in_child(t_cmd *cmd, pid_t *pid, int (*exec_cmd)(const char *,
-			char *const[], char *const[]))
-{
-	bool	is_child;
-
-	*pid = fork();
-	is_child = !(*pid);
-	if (is_child)
-	{
-		close_pipe_fds(cmd->next, PIPE | HEREDOC, -1);
-		if (apply_redirs(cmd->redirs) != 0)
-			exit(errno);
-		if (resolve_command_location(cmd), cmd->location)
-			exec_cmd(cmd->location, cmd->args, ft_getenv_array());
-		print_error(cmd->args[0], strerror(errno));
-		exit(127);
-	}
-	else if (*pid > 0)
-		close_pipe_fds(cmd, PIPE | HEREDOC, 1);
-	else
-	{
-		perror("minishell");
-		set_exit_status(errno);
-	}
-}
-
-int	execute_builtin(const char *pathname, char *const argv[],
-		char *const envp[])
-{
-	int	exit_value;
-
-	(void)envp;
-	if (!ft_strcmp(argv[0], "echo"))
-		echo(argv);
-	else if (!ft_strcmp(argv[0], "cd"))
-		cd(argv);
-	else if (!ft_strcmp(argv[0], "pwd"))
-		pwd();
-	else if (!ft_strcmp(argv[0], "env"))
-		env();
-	else if (!ft_strcmp(argv[0], "exit"))
-		exit_shell(argv);
-	else if (!ft_strcmp(argv[0], "export"))
-		export(argv);
-	else if (!ft_strcmp(argv[0], "unset"))
-		unset(argv);
-	else
-		exit_value = 1;
-	exit_value = 0;
-	if (ft_strcmp(pathname, "child") == 0)
-		exit(exit_value);
-	return (exit_value);
-}
-
-void	execute_in_parent(t_shell *shell, t_cmd *cmd)
-{
-	int	exit_code;
-
-	if (apply_redirs(cmd->redirs) == 0)
-		exit_code = execute_builtin("parent", cmd->args, NULL);
-	else
-		exit_code = errno;
-	dup2(shell->std_in, STDIN_FILENO);
-	dup2(shell->std_out, STDOUT_FILENO);
-	set_exit_status(exit_code);
-	ezg_group_release(EXECUTING);
 }
