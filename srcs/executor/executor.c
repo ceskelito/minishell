88ee132f8ab1a9d3525e563132ea45f06b93b1dd
personal-laptop <ceske@etik.com>
2025-11-20 16:42:37 +0100

    Norminetted executor.c

--- srcs/executor/executor.c
+++ srcs/executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: rceschel <rceschel@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 12:42:14 by rceschel          #+#    #+#             */
-/*   Updated: 2025/11/20 16:16:32 by rceschel         ###   ########.fr       */
+/*   Updated: 2025/11/20 16:42:24 by rceschel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,6 @@
 #ifndef O_CLOEXEC
 # define O_CLOEXEC 0
 #endif
-
-static int	count_cmds(t_cmd *cmd_list)
-{
-	int	count;
-
-	count = 0;
-	while (cmd_list)
-	{
-		count++;
-		cmd_list = cmd_list->next;
-	}
-	return (count);
-}
 
 static void	execute_in_child(t_cmd *cmd, pid_t *pid,
 	int (*exec_cmd)(const char *, char *const[], char *const[]))
@@ -98,24 +85,12 @@ static void	execute_in_parent(t_shell *shell, t_cmd *cmd)
 	ezg_group_release(EXECUTING);
 }
 
-void	executor(t_shell *shell)
+static void	execute_pipeline(t_cmd *cmd, int *exit_code)
 {
-	t_cmd	*cmd;
-	pid_t	*pid;
 	int		num_cmds;
-	int		exit_code;
 	int		i;
+	size_t	*pid;
 
-	if (!shell || !shell->cmd_list)
-		return ;
-	cmd = shell->cmd_list;
-	setup_heredocs(cmd);
-	if (is_builtin(cmd->args[0]) && !cmd->pipe_output)
-	{
-		execute_in_parent(shell, cmd);
-		return ;
-	}
-	open_pipeline_fds(cmd);
 	num_cmds = count_cmds(cmd);
 	pid = malloc(sizeof(pid_t) * num_cmds);
 	i = 0;
@@ -131,6 +106,26 @@ void	executor(t_shell *shell)
 	i = 0;
 	while (i < num_cmds)
 		waitpid(pid[i++], &exit_code, 0);
+}
+
+void	executor(t_shell *shell)
+{
+	t_cmd	*cmd;
+	pid_t	*pid;
+	int		exit_code;
+	int		i;
+
+	if (!shell || !shell->cmd_list)
+		return ;
+	cmd = shell->cmd_list;
+	setup_heredocs(cmd);
+	if (is_builtin(cmd->args[0]) && !cmd->pipe_output)
+	{
+		execute_in_parent(shell, cmd);
+		return ;
+	}
+	open_pipeline_fds(cmd);
+	execute_pipeline(cmd, &exit_code);
 	set_exit_status(exit_code);
 	free(pid);
 }
