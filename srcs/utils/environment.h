89cb88a25076fffb6163077d0fe2266aad8c36b3
ceske-42 <ceske@etik.com>
2025-11-25 12:35:47 +0100

    Almost norminetted all

--- srcs/utils/environment.h
+++ srcs/utils/environment.h
@@ -1,9 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   environment.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rceschel <rceschel@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/25 12:34:56 by rceschel          #+#    #+#             */
+/*   Updated: 2025/11/25 12:35:03 by rceschel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef ENVIRONMENT_H
 # define ENVIRONMENT_H
-# define NOKEY	0
-# define NOVALUE	0
+# define NOKEY 0
+# define NOVALUE 0
 
-enum { GET, SET, UNSET, GET_ARRAY };
+enum
+{
+	GET,
+	SET,
+	UNSET,
+	GET_ARRAY
+};
 
-char    **env_handler(int mode, char *key, char *value);
+char	**env_handler(int mode, char *key, char *value);
 #endif
