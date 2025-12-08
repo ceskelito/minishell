f0893095eaf0e1f1bce4e36fce0929d723c7302e
Rostyslav Dolhoplov <rodolhop@e3r2p6.42roma.it>
2025-12-08 17:10:15 +0100

    brought to norm tokenizer.c

--- srcs/tokenizer/tokenizer.c
+++ srcs/tokenizer/tokenizer.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenizer.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rodolhop <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/08 17:01:11 by rodolhop          #+#    #+#             */
+/*   Updated: 2025/12/08 17:01:15 by rodolhop         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ezgalloc.h"
 #include "minishell.h"
 
@@ -10,9 +22,9 @@ static int	fill_operator_token(t_token *token, char *input)
 	return (ft_strlen(token->value));
 }
 
-t_token	*new_token()
+t_token	*new_token(void)
 {
-	t_token *new;
+	t_token	*new;
 
 	new = ezg_calloc(TOKEN, sizeof(t_token), 1);
 	new->expand_dollar = true;
@@ -20,41 +32,47 @@ t_token	*new_token()
 	return (new);
 }
 
+static int	handle_token(t_token **tokens, char *input, int i, bool *is_hd)
+{
+	t_token	*new;
+	int		gap;
+
+	new = new_token();
+	if (!new)
+		return (-1);
+	if (ft_strchr("|<>", input[i]))
+		gap = fill_operator_token(new, &input[i]);
+	else if (*is_hd)
+		gap = fill_eof_token(new, &input[i]);
+	else
+		gap = fill_word_token(new, &input[i]);
+	if (gap == -1 || !new->value)
+		return (-1);
+	add_token(tokens, new);
+	*is_hd = (new->type & HEREDOC);
+	return (gap);
+}
+
 t_token	*tokenize_input(char *input)
 {
-	t_token			*tokens;
-	t_token			*new;
-	bool			is_heredoc_delimiter;
-	int				token_gap;
-	int				i;
+	t_token	*tokens;
+	bool	is_hd;
+	int		i;
+	int		gap;
 
 	tokens = NULL;
+	is_hd = false;
 	i = 0;
 	while (input[i])
 	{
 		while (ft_isspace(input[i]))
-			(i)++;
+			i++;
 		if (!input[i])
-			break;
-		new = new_token();
-		if (ft_strchr("|<>", input[i]))
-		{
-			token_gap = fill_operator_token(new, &input[i]);
-		}
-		else if (is_heredoc_delimiter)
-		{
-			token_gap = fill_eof_token(new, &input[i]);
-		}
-		else
-		{
-			token_gap = fill_word_token(new, &input[i]);
-		}
-		if (token_gap == -1 || !new->value)
+			break ;
+		gap = handle_token(&tokens, input, i, &is_hd);
+		if (gap < 0)
 			return (NULL);
-		add_token(&tokens, new);
-		is_heredoc_delimiter = (new->type & HEREDOC);
-		new = NULL;
-		i += token_gap;
+		i += gap;
 	}
 	return (tokens);
 }
