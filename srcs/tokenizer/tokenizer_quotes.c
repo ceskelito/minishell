2bc1c7201fbf45ea598ec08482309a6bb659fd56
Rostyslav Dolhoplov <rodolhop@e3r2p6.42roma.it>
2025-12-08 18:21:59 +0100

    quotes

--- srcs/tokenizer/tokenizer_quotes.c
+++ srcs/tokenizer/tokenizer_quotes.c
@@ -1,90 +1,79 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   tokenizer_quotes.c                                 :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: rodolhop <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/08 17:12:31 by rodolhop          #+#    #+#             */
-/*   Updated: 2025/12/08 17:12:43 by rodolhop         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "minishell.h"
 
-static char	*append_char_safe(char *str, char c)
+static char *append_char_safe(char *str, char c)
 {
-	char	*temp;
+    char *temp;
 
-	temp = ft_strjoin_char(str, c);
-	if (!temp)
-		return (NULL);
-	free(str);
-	return (temp);
+    temp = ft_strjoin_char(str, c);
+    if (!temp)
+        return (NULL);
+    free(str);
+    return (temp);
 }
 
-static char	*handle_single_quote(char *input, int *i, char *result)
+// Declare the helper function before use
+static char *if_nclsd_qts(char quote_char, char *result, const char *quote_type)
 {
-	(*i)++;
-	while (input[*i] && input[*i] != '\'')
-	{
-		result = append_char_safe(result, input[*i]);
-		if (!result)
-			return (NULL);
-		(*i)++;
-	}
-	return (if_nclsd_qts(input[*i], result, "'"));
+    if (quote_char != quote_type[0])
+    {
+        printf("minishell: syntax error: unclosed %s\n", quote_type);
+        if (result)
+            free(result);
+        return (NULL);
+    }
+    return (result);
 }
 
-static char	*handle_double_quote(char *input, int *i, char *result)
+static char *handle_single_quote(char *input, int *i, char *result)
 {
-	char	*temp;
-
-	(*i)++;
-	while (input[*i] && input[*i] != '\"')
-	{
-		if (input[*i] == '$')
-		{
-			temp = result;
-			handle_dollar_sign(input, i, &result);
-			if (!result)
-			{
-				if (temp)
-					free(temp);
-				return (NULL);
-			}
-		}
-		else
-		{
-			result = append_char_safe(result, input[*i]);
-			if (!result)
-				return (NULL);
-			(*i)++;
-		}
-	}
-	return (if_nclsd_qts(input[*i], result, "\""));
+    (*i)++;
+    while (input[*i] && input[*i] != '\'')
+    {
+        result = append_char_safe(result, input[*i]);
+        if (!result)
+            return (NULL);
+        (*i)++;
+    }
+    return (if_nclsd_qts(input[*i], result, "'"));
 }
 
-static char	*if_nclsd_qts(char quote_char, char *result, const char *quote_type)
+static char *handle_double_quote(char *input, int *i, char *result)
 {
-	if (quote_char != quote_type[0])
-	{
-		printf("minishell: syntax error: unclosed %s\n", quote_type);
-		if (result)
-			free(result);
-		return (NULL);
-	}
-	return (result);
+    char *temp;
+
+    (*i)++;
+    while (input[*i] && input[*i] != '\"')
+    {
+        if (input[*i] == '$')
+        {
+            temp = result;
+            handle_dollar_sign(input, i, &result);
+            if (!result)
+            {
+                if (temp)
+                    free(temp);
+                return (NULL);
+            }
+        }
+        else
+        {
+            result = append_char_safe(result, input[*i]);
+            if (!result)
+                return (NULL);
+            (*i)++;
+        }
+    }
+    return (if_nclsd_qts(input[*i], result, "\""));
 }
 
-char	*process_quotes(char *input, int *i, char *result)
+char *process_quotes(char *input, int *i, char *result)
 {
-	char	quote;
+    char quote;
 
-	quote = input[*i];
-	if (quote == '\'')
-		return (handle_single_quote(input, i, result));
-	else if (quote == '\"')
-		return (handle_double_quote(input, i, result));
-	return (result);
+    quote = input[*i];
+    if (quote == '\'')
+        return (handle_single_quote(input, i, result));
+    else if (quote == '\"')
+        return (handle_double_quote(input, i, result));
+    return (result);
 }
