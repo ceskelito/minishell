3855b70d0d66a1bbd8de65b02b86b1c75a01cb73
ceske-42 <ceske@etik.com>
2025-12-11 16:22:34 +0100

    Removed unused functions and defines

--- includes/executor.h
+++ includes/executor.h
@@ -7,27 +7,10 @@
 # include <unistd.h>
 # include <stdbool.h>
 
-# define T_ECHO "echo"
-# define T_FLAG_N "-n"
-# define T_CD "cd"
-# define T_PWD "pwd"
-# define T_EXPORT "export"
-# define T_UNSET "unset"
-# define T_ENV "env"
-# define T_EXIT "exit"
-
-t_token 	*create_test_tokens(t_token *token);
 void		executor(t_shell *shell);
 bool		resolve_command_location(t_cmd *cmd);
 void		setup_heredocs(t_cmd *cmd_list);
-void		setup_heredocs(t_cmd *cmd_list);
 int		    open_pipeline_fds(t_cmd *cmd_list);
-
-/* Exec functions*/
-/* void	execute_in_child(t_cmd *cmd, pid_t *pid,
-           	    int (*exec_cmd)(const char *, char *const [], char *const []));
-int     execute_builtin(const char *pathname, char * const argv[], char *const envp[]);
-void    execute_in_parent(t_shell *shell, t_cmd *cmd); */
 
 /* Redirections */
 int		apply_redirs(t_redir *r);
