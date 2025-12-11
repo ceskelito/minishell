462661ccb1d3be68ec75eeaa80da1241f208fe79
personal-laptop <ceske@etik.com>
2025-12-11 11:00:08 +0100

    Moved functions from main.c to specific files

--- srcs/main.c
+++ srcs/main.c
@@ -1,15 +1,27 @@
-#include <stdio.h>              
-#include <readline/readline.h>
-#include <readline/history.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rceschel <rceschel@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/11 10:10:43 by rceschel          #+#    #+#             */
+/*   Updated: 2025/12/11 10:59:27 by rceschel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "executor.h"
 #include "ezgalloc.h"
 #include "ft_lib.h"
 #include "minishell.h"
+#include <readline/history.h>
+#include <readline/readline.h>
 #include <signal.h>
+#include <stdio.h>
 #include <stdio.h>
 #include <unistd.h>
 
-int	g_sig_status = 0;
+int			g_sig_status = 0;
 
 static int	is_debug_command(char *input)
 {
@@ -38,7 +50,7 @@ static void	process_command(char *input, t_shell *shell)
 
 	debug_mode = is_debug_command(input);
 	if (debug_mode)
-		cmd = extract_command(input); // cmd = input -- extract is only for debug ppourposes
+		cmd = extract_command(input);
 	else
 		cmd = input;
 	shell->tokens = tokenize_input(cmd);
@@ -49,106 +61,22 @@ static void	process_command(char *input, t_shell *shell)
 	executor(shell);
 }
 
-void __attribute__((constructor)) create_groups();
-
-void	create_groups()
-{
-	ezg_group_create(TOKEN);
-	ezg_group_create(GLOBAL);
-	ezg_group_create(COMMAND);
-	ezg_group_create(EXECUTING);
-	ezg_group_create(ENV);
-}
-
-static char	*prompt_getcwd(char *home_symbol)
-{
-	char	*home_path;
-	char	*new_directory;
-	char	*directory;
-
-	directory = getcwd(NULL, 0);
-	if (!directory)
-		return (perror("minishell"), NULL);
-	home_path = ft_getenv("HOME");
-	if (!home_path)
-		return (directory);
-	if (ft_strnstr(directory, home_path, ft_strlen(home_path)))
-	{
-		/* allocate new string for prompt (home_symbol + rest of path) */
-		new_directory = ft_calloc(ft_strlen(home_symbol) +
-								ft_strlen(directory + ft_strlen(home_path)) +
-								1,
-								sizeof(char));
-		if (!new_directory)
-			return (directory);
-		ft_sprintf(new_directory, "%s%s", home_symbol, directory + ft_strlen(home_path));
-		free(directory);
-		return (new_directory);
-	}
-	return (directory);
-}
-
-static char    *get_prompt()
-{
-    char    *working_directory;
-    char    *user;
-    char    *prompt;
-    size_t  prompt_len;
-
-    user = ft_getenv("USER");
-	working_directory = prompt_getcwd(HOME_SYMBOL);
-	if (!user)
-		user = "\0";
-	if (!working_directory)
-		working_directory = "\0";
-    prompt_len = ft_strlen(user) +
-                 ft_strlen(working_directory) + 
-                 (ft_strlen(GREEN) * 4) + 
-                 3 + 1;
-    prompt = malloc(sizeof(char) * prompt_len);
-    ft_sprintf(prompt, "%s%s%s%c%s%s%s%c%c",
-                GREEN, user, 
-                DEFAULT, ':',
-                BLUE, working_directory,
-                DEFAULT, '$', ' ');
-    free(working_directory);
-    return (ezg_add(EXECUTING, prompt));
-}
-
-void	handle_sigint(int signal)
-{
-	//extern unsigned long	rl_readline_state;
-	(void)signal;
-	if (isatty(STDIN_FILENO))
-	{
-		write(STDOUT_FILENO, "\n", 1);
-		rl_replace_line("", 0);
-		rl_on_new_line();
-		rl_redisplay();
-		g_sig_status = SIGINT;
-	}
-}
-
-void set_signal(int signum, void (*handler)(int)) {
-    struct sigaction sa;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-    sa.sa_handler = handler;
-    sigaction(signum, &sa, NULL);
-}
+void	create_groups(void) __attribute__((constructor));
+void	set_signal(int signum, void (*handler)(int));
+void	handle_sigint(int signal);
+char	*get_prompt(void);
 
 int	main(void)
 {
-	t_shell				shell;
-	char				*input;
-	char				*cwd;
+	t_shell	shell;
+	char	*input;
+	char	*cwd;
 
 	set_signal(SIGINT, handle_sigint);
 	set_signal(SIGQUIT, SIG_IGN);
 	init_shell(&shell);
 	printf("Welcome to minishell!\n");
 	printf("Type 'DEBUG: command' to see tokenization and parsing.\n\n");
-	
 	while (1)
 	{
 		input = ezg_add(EXECUTING, readline(get_prompt()));
@@ -159,7 +87,7 @@ int	main(void)
 		{
 			perror("minishell");
 			set_exit_status(errno);
-			break;
+			break ;
 		}
 		if (ft_strcmp(cwd, ft_getenv("PWD")) != 0)
 			ft_setenv("PWD", cwd);
---
+++ srcs/utils/get_prompt.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_prompt.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rceschel <rceschel@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/11 10:57:44 by rceschel          #+#    #+#             */
+/*   Updated: 2025/12/11 10:58:21 by rceschel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static char	*prompt_getcwd(char *home_symbol)
+{
+	char	*home_path;
+	char	*new_directory;
+	char	*directory;
+
+	directory = getcwd(NULL, 0);
+	if (!directory)
+		return (perror("minishell"), NULL);
+	home_path = ft_getenv("HOME");
+	if (!home_path)
+		return (directory);
+	if (ft_strnstr(directory, home_path, ft_strlen(home_path)))
+	{
+		new_directory = ft_calloc(ft_strlen(home_symbol) + ft_strlen(directory
+					+ ft_strlen(home_path)) + 1, sizeof(char));
+		if (!new_directory)
+			return (directory);
+		ft_sprintf(new_directory, "%s%s", home_symbol, directory
+			+ ft_strlen(home_path));
+		free(directory);
+		return (new_directory);
+	}
+	return (directory);
+}
+
+char	*get_prompt(void)
+{
+	char	*working_directory;
+	char	*user;
+	char	*prompt;
+	size_t	prompt_len;
+
+	user = ft_getenv("USER");
+	working_directory = prompt_getcwd(HOME_SYMBOL);
+	if (!user)
+		user = "\0";
+	if (!working_directory)
+		working_directory = "\0";
+	prompt_len = ft_strlen(user) + ft_strlen(working_directory)
+		+ (ft_strlen(GREEN) * 4) + 3 + 1;
+	prompt = malloc(sizeof(char) * prompt_len);
+	ft_sprintf(prompt, "%s%s%s%c%s%s%s%c%c", GREEN, user, DEFAULT, ':', BLUE,
+		working_directory, DEFAULT, '$', ' ');
+	free(working_directory);
+	return (ezg_add(EXECUTING, prompt));
+}
